0981b6593f28d937431afd01d3e7212b960df5bb
Vas2113 <ololol2113@mail.ru>
2025-09-02 15:23:19 +0500

    fixStyles

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Canvas } from '@react-three/fiber';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div id="container">
+      <div className="aim" />
+      <Canvas camera={{ fov: 45 }}>
+        <App />
+      </Canvas>
+    </div>
+  </React.StrictMode>
+);
