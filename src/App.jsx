0debdff66dd153c1985a26eb28fd32ba737513d7
Vas2113 <ololol2113@mail.ru>
2025-09-02 15:35:58 +0500

    groundInit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Sky } from '@react-three/drei';
+import { Ground } from './components/Ground';
 
 const App = () => {
   return (
     <>
       <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={1.5} />
+      <Ground />
     </>
   );
 };
